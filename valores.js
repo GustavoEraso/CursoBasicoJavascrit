bf0bb63b702b0e56a10d4d2522b950ac5fe567fa
Gustavo Eraso <gustavoeraso1988@gmail.com>
2022-01-06 05:08:22 -0300

    introduccion a tipos de valores

---
+++ valores.js
@@ -0,0 +1,25 @@
+
+//VALORES PRIMITIVOS
+
+    //NUMERO
+        40 
+    //STRING (cadenas de texto)
+        "Gustavo Eraso"
+
+    //BOOLEANO
+        true = // 1
+        false = // 0 
+
+    //VALOR VACIO
+    null   // ES RARO PORQUE TAMBIEN SE PUEDE COMPORTAR COMO OBJETO 
+    undefined 
+
+ 
+
+//VALORES TIPO OBJETOS
+     
+    //ARAYS
+     [1,2,3]
+
+    //OBJETOS
+    {nombre: "Gustavo"}
